02c3483232419d4adb947589dc76c844b384503f
Obi Ohagwu <obiohagwu8@gmail.com>
2023-01-20 18:23:50 -0500

    test file

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        PointCloud cloud = new PointCloud("clouds/PointCloud1.xyz");
+        Point3D point = cloud.getPoint();
+        cloud.addPoint(new Point3D(1, 2, 3));
+        cloud.save("clouds/new_points.xyz");
+    }
+}
